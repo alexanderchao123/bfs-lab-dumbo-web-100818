4f68f35df49619b0395dd8eaae8cfa6169686a87
Alex Chao <chao.alexander.123@gmail.com>
2019-04-03 16:26:53 +0000

    create findAdjacent function

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+let edges = [
+		['14th&6th', '23rd&6th'],
+		['23rd&6th', '34th&6th'],
+		['34th&6th', '28th&Bwy'],
+		['28th&Bwy', '23rd&Bwy'],
+		['23rd&Bwy', '14th&Lex'],
+		['14th&Lex', '23rd&Lex']
+	]
+
+	let vertices = [
+	  {name: '34th&6th', distance: null, predecessor: null},
+	  {name: '23rd&6th', distance: null, predecessor: null},
+		{name: '28th&Bwy', distance: null, predecessor: null},
+
+	  {name: '14th&6th', distance: null, predecessor: null},
+	  {name: '23rd&Bwy', distance: null, predecessor: null},
+	  {name: '14th&Lex', distance: null, predecessor: null},
+	  {name: '23rd&Lex', distance: null, predecessor: null}
+	]
+
 function bfs(rootNode, vertices, edges){
 
+}
+
+function findAdjacent(vertex,  vertices, edges) {
+  
 }
