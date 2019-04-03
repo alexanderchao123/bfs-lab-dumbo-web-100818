87d63c9e8569f9a649274465ede0fc114fade575
Alex Chao <chao.alexander.123@gmail.com>
2019-04-03 19:40:45 +0000

    create checkVertices function as a helper function for findAdjacent and pass the tests

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
+let edges = [
+		['14th&6th', '23rd&6th'],
+		['23rd&6th', '34th&6th'],
+		['34th&6th', '28th&Bwy'],
+		['28th&Bwy', '23rd&Bwy'],
+		['23rd&Bwy', '14th&Lex'],
+		['14th&Lex', '23rd&Lex']
+	]
+
+	let vertices = [
+	  {name: '34th&6th', distance: null, predecessor: null},
+	  {name: '23rd&6th', distance: null, predecessor: null},
+		{name: '28th&Bwy', distance: null, predecessor: null},
+
+	  {name: '14th&6th', distance: null, predecessor: null},
+	  {name: '23rd&Bwy', distance: null, predecessor: null},
+	  {name: '14th&Lex', distance: null, predecessor: null},
+	  {name: '23rd&Lex', distance: null, predecessor: null}
+	]
+
 function bfs(rootNode, vertices, edges){
 
+}
+
+function checkVertices(predecessor, node, vertices) {
+  for(let i = 0; i < vertices.length; i++) {
+    let vertex = vertices[i]
+    if (vertex["name"] === node && vertex["name"] !== predecessor && vertex["distance"] === null) {
+      console.log(vertex)
+      return vertex
+      }
+  }
+}
+
+function findAdjacent(node,  vertices, edges) {
+  let nodes = []
+  for(let i = 0; i < edges.length; i++){
+    let currentEdge = edges[i]
+    if(currentEdge[0] === node && checkVertices(node, currentEdge[1], vertices)) {
+      nodes.push(checkVertices(node, currentEdge[1], vertices))
+    } else if(currentEdge[1] === node && checkVertices(node, currentEdge[0], vertices)) {
+      nodes.push(checkVertices(node, currentEdge[0], vertices))
+    }
+  }
+  return nodes
 }
